e33710e9a54dac2bbafdf34b0a1d3a3091ec1913
christopher-hague <christopher.a.hague@gmail.com>
2017-03-14 21:18:28 -0700

    submit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade_num)
+    @roster[grade_num]
+  end
+
+  def sort
+    result = {}
+
+    roster.each do |grade, students|
+      result[grade] = students.sort
+    end
+
+    result
+  end
+end
